5ad2c692bccb70ce249107bdc849ba3a94c02542
Daria Zakharova <daria.zakharova1404@gmail.com>
2025-04-16 13:48:07 +0300

    Add form submission test for NewProject component

--- src/components/NewProject.test.jsx
+++ src/components/NewProject.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from '@testing-library/user-event';
 import App from "../App";
+import NewProject from "./NewProject.jsx";
 
 const modalRoot = document.createElement('div');
 modalRoot.setAttribute('id', 'modal-root');
@@ -16,4 +17,34 @@ describe('NewProject component', () => {
     const titleInput = screen.getByLabelText(/title/i);
     expect(titleInput).toBeInTheDocument();
   });
+
+    test('onAdd is called when form is submitted', async () => {
+      const handleAddProject = vi.fn();
+
+      render(<NewProject onAdd={handleAddProject} onCancel={() => {}} />);
+
+
+
+      const titleInput = screen.getByLabelText(/title/i);
+      const descriptionInput = screen.getByLabelText(/description/i);
+      const dateInput = screen.getByLabelText(/due date/i);
+
+      await userEvent.type(titleInput, 'My New Project');
+      await userEvent.type(descriptionInput, 'This is a description');
+      await userEvent.type(dateInput, '2024-04-16');
+
+      const saveButton = screen.getByRole('button', {name: /save/i});
+      await saveButton.click();
+
+      expect(handleAddProject).toHaveBeenCalledTimes(1);
+
+      expect(handleAddProject).toHaveBeenCalledWith({
+        title: 'My New Project',
+        description: 'This is a description',
+        dueDate: '2024-04-16',
+      });
+
+    });
+
+
 })
