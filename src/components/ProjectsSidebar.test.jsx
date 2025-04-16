02a4f1cc5b43f50f64bdff0ad548609e3cfd3869
Daria Zakharova <daria.zakharova1404@gmail.com>
2025-04-16 12:32:46 +0300

    Add test to ensure all project titles are rendered in ProjectsSidebar

--- src/components/ProjectsSidebar.test.jsx
+++ src/components/ProjectsSidebar.test.jsx
@@ -1,8 +1,27 @@
 import { render, screen } from "@testing-library/react";
 import ProjectsSidebar from "./ProjectsSidebar";
 
-test('renders YOUR PROJECTS', () => {
-    render(<ProjectsSidebar projects={[]}/>);
-    const h2elem = screen.getByText('Your Projects');
-    expect(h2elem).toBeInTheDocument();
-});
+describe('ProjectSidebar component', () => {
+    test('renders YOUR PROJECTS', () => {
+        render(<ProjectsSidebar projects={[]}/>);
+        const h2elem = screen.getByText('Your Projects');
+        expect(h2elem).toBeInTheDocument();
+    });
+
+    test('renders YOUR PROJECTS TITLES', () => {
+        const testProjects = [
+            {id: 1,
+            title: 'Project 1'},
+            {id: 2,
+            title: 'Project 2'},
+            {id: 3,
+            title: 'Project 3'},
+        ]
+
+        render(<ProjectsSidebar projects={testProjects} />);
+
+        testProjects.forEach(project => {
+            expect(screen.getByText(project.title)).toBeInTheDocument();
+        })
+    })
+})
